0971420deac2bdc882e80370b3c75f511ebf5639
Jianhong Ou <jianhong.ou@umassmed.edu>
2015-03-19 17:26:04 +0000

    export $ and $<- methods for class PASclassifier, modelInfo and featureVector.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cleanUpdTSeq@100878 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -4,13 +4,41 @@ setClass("modelInfo", representation(upstream="integer",
                                      alphabet="character",
                                      genome="BSgenome"))
 
+setMethod("$", "modelInfo", function(x, name) slot(x, name))
+setReplaceMethod("$", "modelInfo", 
+                 function(x, name, value){
+                     slot(x, name, check = TRUE) <- value
+                     x
+                 })
+
 setClass("naiveBayes", representation(apriori="table",
                                       tables="list",
                                       levels="NULL",
                                       call="call"))
 
+setMethod("$", "naiveBayes", function(x, name) slot(x, name))
+setReplaceMethod("$", "naiveBayes", 
+                 function(x, name, value){
+                     slot(x, name, check = TRUE) <- value
+                     x
+                 })
+
 setClass("PASclassifier", representation(classifier="naiveBayes", 
                                          info="modelInfo"))
 
+setMethod("$", "PASclassifier", function(x, name) slot(x, name))
+setReplaceMethod("$", "PASclassifier", 
+                 function(x, name, value){
+                     slot(x, name, check = TRUE) <- value
+                     x
+                 })
+
 setClass("featureVector", representation(data="data.frame", 
                                          info="modelInfo"))
+
+setMethod("$", "featureVector", function(x, name) slot(x, name))
+setReplaceMethod("$", "featureVector", 
+                 function(x, name, value){
+                     slot(x, name, check = TRUE) <- value
+                     x
+                 })
